85ee42225583d2f9c4cc298b6a6536d32f5da849
Francois Dabonot <frankois@azodus.fr.nf>
2018-01-03 19:16:01 +0100

    Add empty sample, work in progress...

---
+++ Sample/OBJ-C/Sample/Sample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Sample
+//
+//  Created by Dabonot Francois on 03/01/2018.
+//  Copyright © 2018 Dabonot Francois. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Sample/OBJ-C/Sample/Sample/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  Sample
+//
+//  Created by Dabonot Francois on 03/01/2018.
+//  Copyright © 2018 Dabonot Francois. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <MvvmForIOS/MvvmForIOS.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
